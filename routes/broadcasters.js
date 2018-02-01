c8099d6c058dbf92a3aa4dfe930c17ea1f8e761b
Arnelle Balane <arnellebalane@gmail.com>
2018-02-01 11:40:47 +0800

    add route for generating sessions and publisher tokens

--- routes/broadcasters.js
+++ routes/broadcasters.js
@@ -1,10 +1,36 @@
 const { get } = require('server/router');
-const { render } = require('server/reply');
+const { json, render } = require('server/reply');
+const opentok = require('../lib/opentok');
+
+const sessions = {};
 
 function index(ctx) {
     return render('broadcaster.html');
 }
 
+async function session(ctx) {
+    const id = ctx.params.id;
+
+    const response = await new Promise((resolve, reject) => {
+        opentok.createSession((err, session) => {
+            const token = session.generateToken({
+                role: 'publisher',
+                expireTime: (Date.now() / 1000) + (24 * 60 * 60),
+                data: `user=${id}`
+            });
+            sessions[id] = { session, token };
+
+            resolve({
+                apiKey: process.env.OPENTOK_API_KEY,
+                sessionId: session.sessionId,
+                token: token
+            });
+        });
+    });
+    return json(response);
+}
+
 module.exports = [
-    get('/broadcaster/:id', index)
+    get('/broadcaster/:id', index),
+    get('/broadcaster/:id/session', session)
 ];
