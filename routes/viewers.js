52ef2ad919630dd2f6e5be63f10d1ed01bc3117b
Arnelle Balane <arnellebalane@gmail.com>
2018-02-01 12:20:31 +0800

    add route for obtaining viewer session info

--- routes/viewers.js
+++ routes/viewers.js
@@ -1,10 +1,22 @@
 const { get } = require('server/router');
-const { render } = require('server/reply');
+const { json, render } = require('server/reply');
+const store = require('../lib/store');
 
 function index(ctx) {
     return render('viewer.html');
 }
 
+function session(ctx) {
+    const id = ctx.params.id;
+    const { session } = store.get(id);
+
+    return json({
+        apiKey: process.env.OPENTOK_API_KEY,
+        sessionId: session.sessionId
+    });
+}
+
 module.exports = [
-    get('/viewer/:id', index)
+    get('/viewer/:id', index),
+    get('/viewer/:id/session', session)
 ];
